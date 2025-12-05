6f69d72bdead7263f0ada5b0d1b065f648ef0a93
Mukesh <mukeshdixena123@gmail.com>
2025-12-06 04:30:59 +0530

    Fix redirect if user don't have token

--- chat-client/src/app/app.routes.ts
+++ chat-client/src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
-import { ChatComponent } from './chat/chat.component';
 import { ChatLayoutComponent } from './components/chat-layout/chat-layout.component';
 import { ProfileComponent } from './profile/profile.component';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
   { path: "", redirectTo: "login", pathMatch: "full" },
   { path: "login", component: LoginComponent },
   { path: "signup", component: SignupComponent },
-  { path: "chat", component: ChatLayoutComponent },
-  { path: "profile", component: ProfileComponent }
+  { path: "chat", component: ChatLayoutComponent, canActivate: [authGuard] },
+  { path: "profile", component: ProfileComponent, canActivate: [authGuard] }
 ];
